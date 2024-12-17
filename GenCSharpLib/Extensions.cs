5ae7ab845fa576cba5147990ea13f06913acb425
TiLied <TiLied@users.noreply.github.com>
2024-12-17 18:00:28 +0300

    Add project files.

---
+++ GenCSharpLib/Extensions.cs
@@ -0,0 +1,284 @@
+﻿using System.Collections;
+using System.Reflection;
+
+namespace GenCSharpLib
+{
+	internal static class Extensions
+	{
+		//https://stackoverflow.com/a/21755933
+		public static string? FirstCharToLowerCase(this string? str)
+		{
+			if (!string.IsNullOrEmpty(str) && char.IsUpper(str[0]))
+				return str.Length == 1 ? char.ToLower(str[0]).ToString() : char.ToLower(str[0]) + str[1..];
+
+			return str;
+		}
+		public static string? FirstCharToUpperCase(this string? str)
+		{
+			if (!string.IsNullOrEmpty(str) && char.IsLower(str[0]))
+				return str.Length == 1 ? char.ToUpper(str[0]).ToString() : char.ToUpper(str[0]) + str[1..];
+
+			return str;
+		}
+
+		public static bool Contains(this string source, string toCheck, StringComparison comp)
+		{
+			return source?.IndexOf(toCheck, comp) >= 0;
+		}
+
+		public static string Test(this Object obj) 
+		{
+			return "sad";
+		}
+
+		/*
+		public static T Clone<T>(T source)
+		{
+			JsonSerializerOptions serializeOptions = new();
+			var serialized = JsonSerializer.Serialize(source, serializeOptions);
+			return JsonSerializer.Deserialize<T>(serialized);
+		}*/
+
+		public static object CloneObject(this object objSource)
+		{
+			//Get the type of source object and create a new instance of that type
+			Type typeSource = objSource.GetType();
+			object objTarget = Activator.CreateInstance(typeSource);
+			//Get all the properties of source object type
+			PropertyInfo[] propertyInfo = typeSource.GetProperties(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+			//Assign all source property to taget object 's properties
+			foreach (PropertyInfo property in propertyInfo)
+			{
+				//Check whether property can be written to
+				if (property.CanWrite && property.Name != "Item")
+				{
+					//check whether property type is value type, enum or string type
+					if (property.PropertyType.IsValueType || property.PropertyType.IsEnum || property.PropertyType.Equals(typeof(string)))
+					{
+						property.SetValue(objTarget, property.GetValue(objSource, null), null);
+					}
+					//else property type is object/complex types, so need to recursively call this method until the end of the tree is reached
+					else
+					{
+						object objPropertyValue = null;
+						if (property.GetIndexParameters().Length > 0)
+						{
+							int _length = (objSource as IList).Count;
+
+							for (int i = 0; i < _length; i++)
+							{
+								objPropertyValue = property.GetValue(objSource, new object[] { (int)i });
+								
+								objTarget.GetType().GetMethod("Add").Invoke(objTarget, new object[] { null });
+
+								if (objPropertyValue == null)
+								{
+									property.SetValue(objTarget, null, new object[] { (int)i });
+								}
+								else
+								{
+									property.SetValue(objTarget, objPropertyValue.CloneObject(), new object[] { (int)i });
+								}
+							}
+						}
+						else
+						{
+							objPropertyValue = property.GetValue(objSource, null);
+						
+
+							if (objPropertyValue == null)
+							{
+								property.SetValue(objTarget, null, null);
+							}
+							else
+							{
+								property.SetValue(objTarget, objPropertyValue.CloneObject(), null);
+							}
+						}
+					}
+				}
+			}
+			return objTarget;
+		}
+
+		public static T DeepCloneByReflection<T>(this T source)
+		{
+			var type = source.GetType();
+
+			var target = Activator.CreateInstance(type);
+
+			foreach (var propertyInfo in type.GetProperties())
+			{
+				if (propertyInfo.CanWrite && propertyInfo.CanRead)
+				{
+					// Handle value types and string
+					if (propertyInfo.PropertyType.IsValueType ||
+					propertyInfo.PropertyType == typeof(string))
+					{
+						propertyInfo.SetValue(target, propertyInfo.GetValue(source));
+					}
+					// Handle delegates
+					else if (propertyInfo.PropertyType.IsSubclassOf(typeof(Delegate)))
+					{
+						var value = (Delegate)propertyInfo.GetValue(source);
+
+						if (value != null)
+						{
+							propertyInfo.SetValue(target, value.Clone());
+						}
+					}
+					// Handle arrays
+					else if (propertyInfo.PropertyType.IsSubclassOf(typeof(Array)))
+					{
+						var value = (Array)propertyInfo.GetValue(source);
+
+						if (value != null)
+						{
+							propertyInfo.SetValue(target, value.Clone());
+						}
+					}
+					// Handle objects
+					else
+					{
+						var value = propertyInfo.GetValue(source);
+
+						if (value != null)
+						{
+							propertyInfo.SetValue(target, value.DeepCloneByReflection());
+						}
+					}
+				}
+			}
+
+			return (T)target;
+		}
+
+	}
+
+	/// <summary>
+	/// BaseObject class is an abstract class for you to derive from.
+	/// Every class that will be dirived from this class will support the 
+	/// Clone method automaticly.<br>
+	/// The class implements the interface ICloneable and there 
+	/// for every object that will be derived <br>
+	/// from this object will support the ICloneable interface as well.
+	/// </summary>
+
+	public abstract class BaseObject : ICloneable
+	{
+		/// <summary>
+		/// Clone the object, and returning a reference to a cloned object.
+		/// </summary>
+		/// <returns>Reference to the new cloned 
+		/// object.</returns>
+		public object Clone()
+		{
+			//First we create an instance of this specific type.
+			object newObject = Activator.CreateInstance(this.GetType());
+
+			//We get the array of fields for the new type instance.
+			PropertyInfo[] fields = newObject.GetType().GetProperties();
+
+			int i = 0;
+
+			foreach (PropertyInfo fi in this.GetType().GetProperties())
+			{
+				//We query if the fiels support the ICloneable interface.
+				Type ICloneType = fi.PropertyType.GetInterface("ICloneable", true);
+
+				if (ICloneType != null)
+				{
+					//Getting the ICloneable interface from the object.
+					ICloneable IClone = (ICloneable)fi.GetValue(this);
+
+					//We use the clone method to set the new value to the field.
+					fields[i].SetValue(newObject, IClone.Clone());
+				}
+				else
+				{
+					// If the field doesn't support the ICloneable 
+					// interface then just set it.
+					fields[i].SetValue(newObject, fi.GetValue(this));
+				}
+
+				//Now we check if the object support the 
+				//IEnumerable interface, so if it does
+				//we need to enumerate all its items and check if 
+				//they support the ICloneable interface.
+				Type IEnumerableType = fi.PropertyType.GetInterface
+								("IEnumerable", true);
+				if (IEnumerableType != null)
+				{
+					//Get the IEnumerable interface from the field.
+					IEnumerable IEnum = (IEnumerable)fi.GetValue(this);
+
+					//This version support the IList and the 
+					//IDictionary interfaces to iterate on collections.
+					Type IListType = fields[i].PropertyType.GetInterface
+										("IList", true);
+					Type IDicType = fields[i].PropertyType.GetInterface
+										("IDictionary", true);
+
+					int j = 0;
+					if (IListType != null)
+					{
+						//Getting the IList interface.
+						IList list = (IList)fields[i].GetValue(newObject);
+
+						foreach (object obj in IEnum)
+						{
+							//Checking to see if the current item 
+							//support the ICloneable interface.
+							ICloneType = obj.GetType().
+								GetInterface("ICloneable", true);
+
+							if (ICloneType != null)
+							{
+								//If it does support the ICloneable interface, 
+								//we use it to set the clone of
+								//the object in the list.
+								ICloneable clone = (ICloneable)obj;
+
+								list[j] = clone.Clone();
+							}
+
+							//NOTE: If the item in the list is not 
+							//support the ICloneable interface then in the 
+							//cloned list this item will be the same 
+							//item as in the original list
+							//(as long as this type is a reference type).
+
+							j++;
+						}
+					}
+					else if (IDicType != null)
+					{
+						//Getting the dictionary interface.
+						IDictionary dic = (IDictionary)fields[i].
+											GetValue(newObject);
+						j = 0;
+
+						foreach (DictionaryEntry de in IEnum)
+						{
+							//Checking to see if the item 
+							//support the ICloneable interface.
+							ICloneType = de.Value.GetType().
+								GetInterface("ICloneable", true);
+
+							if (ICloneType != null)
+							{
+								ICloneable clone = (ICloneable)de.Value;
+
+								dic[de.Key] = clone.Clone();
+							}
+							j++;
+						}
+					}
+				}
+				i++;
+			}
+			return newObject;
+		}
+	}
+
+}
---
+++ GenCSharpLib/JsonClasses.cs
@@ -0,0 +1,361 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System;
+
+namespace GenCSharpLib
+{
+	public partial class Welcome
+	{
+		public List<TType> TType { get; set; }
+
+		public Welcome() { }
+	}
+
+	public class WebIDLType
+	{
+		[JsonPropertyName("type")]
+		public string? Type { get; set; }
+		[JsonPropertyName("generic")]
+		public string? Generic { get; set; }
+		[JsonIgnore]
+		public List<WebIDLType>? IDLType { get; set; }
+		[JsonPropertyName("idlType")]
+		public string? IDLTypeStr { get; set; }
+		[JsonPropertyName("nullable")]
+		public bool? Nullable { get; set; }
+		[JsonPropertyName("union")]
+		public bool? Union { get; set; }
+		[JsonPropertyName("extAttrs")]
+		public List<ExtAttr?> ExtAttrs { get; set; }
+
+		//[JsonExtensionData]
+		//public Dictionary<string, JsonElement>? ExtensionData { get; set; } = new();
+
+		public WebIDLType() { }
+
+		public static bool Equals(WebIDLType obj)
+		{
+			//Check for null and compare run-time types.
+			if (obj.Nullable == null &&
+				obj.Type == null &&
+				obj.Generic == null &&
+				obj.ExtAttrs == null &&
+				obj.Union == null &&
+				obj.IDLTypeStr == null &&
+				obj.IDLType == null)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+	}
+
+	public class TType
+	{
+		[JsonIgnore]
+		public List<TType> ListAdditionalInheritance { get; set; } = new();
+		[JsonPropertyName("type")]
+		public string? Type { get; set; }
+		[JsonPropertyName("name")]
+		public string? Name { get; set; }
+		[JsonPropertyName("inheritance")]
+		public string? Inheritance { get; set; }
+		[JsonPropertyName("partial")]
+		public bool? Partial { get; set; }
+		[JsonPropertyName("members")]
+		public List<Member> Members { get; set; }
+		[JsonPropertyName("extAttrs")]
+		public List<ExtAttr?> ExtAttrs { get; set; }
+
+		[JsonConverter(typeof(IDLTypeJsonConverter))]
+		[JsonPropertyName("idlType")]
+		public List<WebIDLType>? IDLType { get; set; }
+		[JsonPropertyName("arguments")]
+		public List<Argument>? Arguments { get; set; }
+
+		[JsonPropertyName("values")]
+		public List<Value>? Values { get; set; }
+
+		[JsonPropertyName("target")]
+		public string? Target { get; set; }
+		[JsonPropertyName("includes")]
+		public string? Includes { get; set; }
+
+		[JsonExtensionData]
+		public Dictionary<string, JsonElement>? ExtensionData { get; set; }
+
+		public TType() { }
+	}
+
+	public class Member 
+	{
+		[JsonPropertyName("type")]
+		public string? Type { get; set; }
+		[JsonPropertyName("special")]
+		public string? Special { get; set; }
+		[JsonConverter(typeof(IDLTypeJsonConverter))]
+		[JsonPropertyName("idlType")]
+		public List<WebIDLType>? IDLType { get; set; }
+		[JsonPropertyName("name")]
+		public string? Name { get; set; }
+		[JsonPropertyName("arguments")]
+		public List<Argument>? Arguments { get; set; }
+		[JsonPropertyName("extAttrs")]
+		public List<ExtAttr?> ExtAttrs { get; set; }
+		[JsonPropertyName("parent")]
+		public object Parent { get; set; }
+
+		[JsonPropertyName("readonly")]
+		public bool? Readonly { get; set; }
+
+		[JsonPropertyName("value")]
+		public Value? Value { get; set; }
+
+		[JsonPropertyName("async")]
+		public bool? Async { get; set; }
+
+		[JsonPropertyName("default")]
+		public Default? Default { get; set; }
+
+		[JsonPropertyName("required")]
+		public bool? Required { get; set; }
+		[JsonExtensionData]
+		public Dictionary<string, JsonElement>? ExtensionData { get; set; }
+
+		public Member() { }
+	}
+
+	public class Argument
+	{
+		[JsonPropertyName("type")]
+		public string? Type { get; set; }
+		[JsonPropertyName("default")]
+		public Default? Default { get; set; }
+		[JsonPropertyName("optional")]
+		public bool? Optional { get; set; }
+		[JsonPropertyName("variadic")]
+		public bool? Variadic { get; set; }
+		[JsonPropertyName("extAttrs")]
+		public List<ExtAttr?> ExtAttrs { get; set; }
+		[JsonConverter(typeof(IDLTypeJsonConverter))]
+		[JsonPropertyName("idlType")]
+		public List<WebIDLType>? IDLType { get; set; }
+		[JsonPropertyName("name")]
+		public string? Name { get; set; }
+		[JsonPropertyName("parent")]
+		public object Parent { get; set; }
+
+		[JsonExtensionData]
+		public Dictionary<string, JsonElement>? ExtensionData { get; set; }
+
+		public Argument() { }
+	}
+
+	public class ExtAttr
+	{
+		[JsonPropertyName("name")]
+		public string? Name { get; set; }
+		[JsonPropertyName("arguments")]
+		public List<Argument>? Arguments { get; set; }
+		[JsonPropertyName("type")]
+		public string? Type { get; set; }
+		[JsonPropertyName("rhs")]
+		public Rhs? Rhs { get; set; }
+		[JsonPropertyName("parent")]
+		public object Parent { get; set; }
+
+		[JsonExtensionData]
+		public Dictionary<string, JsonElement>? ExtensionData { get; set; }
+
+		public ExtAttr() { }
+	}
+
+	public class Value
+	{
+		[JsonPropertyName("type")]
+		public string? Type { get; set; }
+		[JsonPropertyName("value")]
+		public object? ValueObj { get; set; }
+
+		[JsonExtensionData]
+		public Dictionary<string, JsonElement>? ExtensionData { get; set; }
+
+		public Value() { }
+	}
+	public class Rhs
+	{
+		[JsonPropertyName("type")]
+		public string? Type { get; set; }
+		[JsonPropertyName("value")]
+		public object? Value { get; set; }
+
+		[JsonExtensionData]
+		public Dictionary<string, JsonElement>? ExtensionData { get; set; }
+
+		public Rhs() { }
+	}
+	public class Default
+	{
+		[JsonPropertyName("type")]
+		public string? Type { get; set; }
+		[JsonPropertyName("value")]
+		public object? Value { get; set; }
+
+		[JsonExtensionData]
+		public Dictionary<string, JsonElement>? ExtensionData { get; set; }
+
+		public Default() { }
+
+		public object Clone()
+		{
+			return MemberwiseClone();
+		}
+	}
+	public class IDLTypeJsonConverter : JsonConverter<List<WebIDLType>>
+	{
+		public override List<WebIDLType> Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+		{
+			List<WebIDLType> list = new();
+			bool array = false;
+
+			if (reader.TokenType == JsonTokenType.StartArray)
+			{
+				list.Add(new());
+				array = true;
+			}
+			
+			if (reader.TokenType == JsonTokenType.StartObject)
+			{
+				list.Add(new());
+			}
+
+			while (reader.Read())
+			{
+				if (reader.TokenType == JsonTokenType.EndArray)
+				{
+					if (array == true)
+					{
+						//TODO?
+						if (WebIDLType.Equals(list.Last()))
+							list.RemoveAt(list.Count - 1);
+
+						return list;
+					}
+				}
+
+				if (reader.TokenType == JsonTokenType.EndObject)
+				{
+					if (array == false)
+						return list;
+					else
+						list.Add(new());
+				}
+
+				if (reader.TokenType == JsonTokenType.PropertyName)
+				{
+					string? propertyName = reader.GetString();
+					switch (propertyName)
+					{
+						case "type":
+							{
+								reader.Read();
+								list.Last().Type = reader.GetString();
+								break;
+							}
+						case "generic":
+							{
+								reader.Read();
+								list.Last().Generic = reader.GetString();
+								break;
+							}
+						case "idlType":
+							{
+								reader.Read();
+								if (reader.TokenType == JsonTokenType.StartArray)
+									list.Last().IDLType = Read(ref reader, typeToConvert, options);
+								else
+									list.Last().IDLTypeStr = reader.GetString();
+								break;
+							}
+						case "nullable":
+							{
+								reader.Read();
+								list.Last().Nullable = reader.GetBoolean();
+								break;
+							}
+						case "union":
+							{
+								reader.Read();
+								list.Last().Union = reader.GetBoolean();
+								break;
+							}
+						case "extAttrs":
+							{
+								//TODO?
+								//
+								//
+								reader.Read();
+								list.Last().ExtAttrs = null;
+								reader.Skip();
+								//reader.Read();
+								break;
+							}
+						default:
+							//reader.Read();
+							//list.Last().ExtensionData.Add(propertyName, JsonDocument.ParseValue(ref reader).RootElement);
+							break;
+					}
+				}
+			}
+
+			return null;
+		}
+
+		public override void Write(Utf8JsonWriter writer, List<WebIDLType> value, JsonSerializerOptions options)
+		{
+			writer.WriteStartArray();
+
+			Write2(ref writer, value, options);
+
+			writer.WriteEndArray();
+		}
+
+		public void Write2(ref Utf8JsonWriter writer, List<WebIDLType> value, JsonSerializerOptions options)
+		{
+			foreach (WebIDLType webIDLType in value)
+			{
+				writer.WriteStartObject();
+
+				writer.WriteString("type", webIDLType.Type);
+				writer.WriteString("generic", webIDLType.Generic);
+
+				writer.WritePropertyName("idlType");
+				if (webIDLType.IDLType != null)
+				{
+					writer.WriteStartArray();
+					//JsonSerializer.Serialize(writer, webIDLType.IDLType, options);
+					Write2(ref writer, webIDLType.IDLType, options);
+					writer.WriteEndArray();
+				}
+				else
+					writer.WriteStringValue(webIDLType.IDLTypeStr);
+
+				writer.WriteBoolean("nullable", (bool)webIDLType.Nullable);
+				writer.WriteBoolean("union", (bool)webIDLType.Union);
+				//
+				//TODO!
+				writer.WriteNull("extAttrs");
+
+				writer.WriteEndObject();
+			}
+		}
+	}
+}
+
+
+
